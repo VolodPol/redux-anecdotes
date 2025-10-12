516f1ebdd970c81f48aea6e0d6820ae6661cf9e0
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-10-12 20:01:23 +0300

    Update base

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
 
-  const vote = (id) => {
+  const vote = id => {
     console.log('vote', id)
   }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
       <form>
-        <div><input /></div>
+        <div>
+          <input />
+        </div>
         <button>create</button>
       </form>
     </div>
