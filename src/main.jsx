887310a51bdc44849571bdd6f19de99f62c3022b
Vova Polishchuk <volodimirpolishchuck@gmail.com>
2025-10-24 19:42:29 +0300

    update 6.3: vote functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
@@ -8,7 +8,7 @@ import reducer from './reducers/anecdoteReducer'
 const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 )
